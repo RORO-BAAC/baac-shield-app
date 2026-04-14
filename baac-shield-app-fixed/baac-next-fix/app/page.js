b706ccb09d4d808ccdcbb201936ba17274f04768
RORO-BAAC <rod.gonzalez@baacconstruction.com>
2026-04-14 12:20:53 -0600

    Update page.js

--- baac-shield-app-fixed/baac-next-fix/app/page.js
+++ baac-shield-app-fixed/baac-next-fix/app/page.js
@@ -1,9 +1,13 @@
 "use client";
 
 import { useEffect, useMemo, useRef, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
 import jsPDF from "jspdf";
 import SignatureCanvas from "react-signature-canvas";
-
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
