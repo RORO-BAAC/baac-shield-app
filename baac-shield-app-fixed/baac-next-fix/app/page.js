4e9ec1f5c5d96a108fafd21b6254b3846e7450b0
RORO-BAAC <rod.gonzalez@baacconstruction.com>
2026-04-17 10:48:13 -0600

    Update page.js

--- baac-shield-app-fixed/baac-next-fix/app/page.js
+++ baac-shield-app-fixed/baac-next-fix/app/page.js
@@ -358,7 +358,6 @@ export default function Home() {
             : "BAAC SHIELD - New Record Submitted",
           worker,
           supervisor,
-          jobSite,
           task,
           risk,
           notes,
