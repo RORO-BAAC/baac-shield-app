5db19469a96e3b2af89bb64fd4cc0dd3053fb92a
RORO-BAAC <rod.gonzalez@baacconstruction.com>
2026-05-14 14:39:36 -0600

    Update page.js

--- baac-shield-app-fixed/baac-next-fix/app/page.js
+++ baac-shield-app-fixed/baac-next-fix/app/page.js
@@ -1578,7 +1578,7 @@ const criticalHazardCount = hazardReports.filter(
 >
   <h3 style={{ marginTop: 0 }}>Hazard Reports Pending Review</h3>
 
-{hazardReports.filter((r) => .filter((r) => r.action_status !== "Closed").length === 0 ? (
+{hazardReports.filter((r) => r.action_status !== "Closed").length === 0 ? (
   <div>No hazard reports pending review.</div>
 ) : (
   hazardReports
