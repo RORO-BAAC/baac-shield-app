73eca289acafcdb845b9705c01c7ab57900066ad
RORO-BAAC <rod.gonzalez@baacconstruction.com>
2026-06-17 14:28:04 -0600

    Update page.js

--- baac-shield-app-fixed/baac-next-fix/app/page.js
+++ baac-shield-app-fixed/baac-next-fix/app/page.js
@@ -685,7 +685,7 @@ async function saveCompanyName() {
       const fileName = `${Date.now()}-${safeName}`;
 
       const uploadRes = await fetch(
-      `${SUPABASE_URL}/storage/v1/object/hazard-photos/${fileName}`,
+ `${SUPABASE_URL}/storage/v1/object/HAZARD-PHOTOS/${fileName}`
         {
           method: "POST",
           headers: {
@@ -704,7 +704,7 @@ async function saveCompanyName() {
       }
 
       uploadedUrls.push(
-       `${SUPABASE_URL}/storage/v1/object/public/hazard-photos/${fileName}`
+`${SUPABASE_URL}/storage/v1/object/public/HAZARD-PHOTOS/${fileName}`
       );
     }
 
