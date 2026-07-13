519d33b26b03771641d8c697de4351640e433507
RORO-BAAC <rod.gonzalez@baacconstruction.com>
2026-07-13 10:28:25 -0600

    Update page.js

--- baac-shield-app-fixed/baac-next-fix/app/page.js
+++ baac-shield-app-fixed/baac-next-fix/app/page.js
@@ -8,7 +8,7 @@ import SignatureCanvas from "react-signature-canvas";
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-  
+   
 function SignatureBox({ sigRef, onSave }) {
   return (
     <div>
