94567da5145e934bd38f0404ba96ce2ca4740590
RORO-BAAC <rod.gonzalez@baacconstruction.com>
2026-07-14 09:20:05 -0600

    Update page.js

--- baac-shield-app-fixed/baac-next-fix/app/page.js
+++ baac-shield-app-fixed/baac-next-fix/app/page.js
@@ -11055,7 +11055,7 @@ setHazardDueDate(report.due_date || "");
 
 {activeTab === "qaqc" &&
   qaqcSection === "telecom" &&
- qaqcInspectionType === "work-inspection"
+  qaqcInspectionType === "work-inspection" && (
     <div
       style={{
         display: "grid",
