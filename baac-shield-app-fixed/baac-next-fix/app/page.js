159ef520a2772557714196e80fc371ea864f7754
RORO-BAAC <rod.gonzalez@baacconstruction.com>
2026-08-07 11:16:33 -0600

    Update page.js

--- baac-shield-app-fixed/baac-next-fix/app/page.js
+++ baac-shield-app-fixed/baac-next-fix/app/page.js
@@ -2309,7 +2309,8 @@ if (vehiclePreUsePhotos.length > 0) {
     immediate_action_taken: vehiclePreUseForm.defectsFound
       ? vehiclePreUseForm.immediateActionTaken
       : "",
-      safe_to_operate: vehiclePreUseForm.safeToOperate,
+     supervisor_notified: vehiclePreUseForm.supervisorNotified,
+   safe_to_operate: vehiclePreUseForm.safeToOperate,
     vehicle_out_of_service: vehiclePreUseForm.vehicleOutOfService,
     out_of_service_reason: vehiclePreUseForm.vehicleOutOfService
       ? vehiclePreUseForm.outOfServiceReason
