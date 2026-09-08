baf4fd6d6b56c74fd54a4b74718bdf2772cc0852
RORO-BAAC <rod.gonzalez@baacconstruction.com>
2026-09-08 14:02:07 -0600

    Update page.js

--- baac-shield-app-fixed/baac-next-fix/app/page.js
+++ baac-shield-app-fixed/baac-next-fix/app/page.js
@@ -668,6 +668,7 @@ const [qaqcFibreTestWavelength, setQaqcFibreTestWavelength] = useState("");
 const [qaqcFibreTestEquipment, setQaqcFibreTestEquipment] = useState("");
 const [qaqcFibreTestEquipmentSerial, setQaqcFibreTestEquipmentSerial] = useState("");
 const [qaqcFibreTestCalibrationCurrent, setQaqcFibreTestCalibrationCurrent] = useState("");
+ const [qaqcFibreTestAcceptanceCriteriaMet, setQaqcFibreTestAcceptanceCriteriaMet] = useState("");     
 const [qaqcFibreTestResult, setQaqcFibreTestResult] = useState("");
 const [qaqcFibreTestDeficiencies, setQaqcFibreTestDeficiencies] = useState("");
 const [qaqcFibreTestAssignedTo, setQaqcFibreTestAssignedTo] = useState("");
