bf1151645f6767daf3eb941547ace351531c0327
RORO-BAAC <rod.gonzalez@baacconstruction.com>
2026-09-08 11:50:29 -0600

    Update page.js

--- baac-shield-app-fixed/baac-next-fix/app/page.js
+++ baac-shield-app-fixed/baac-next-fix/app/page.js
@@ -17920,6 +17920,7 @@ onChange={(e) =>
       <div><strong>Approved Splice Plan / Fibre Assignment Verified:</strong><br />{selectedQaqcSpliceRecord.splice_plan_verified || "-"}</div>
       <div><strong>Closure / Enclosure Properly Sealed:</strong><br />{selectedQaqcSpliceRecord.closure_sealed || "-"}</div>
       <div><strong>Fibre Identification Matches Splice Schedule:</strong><br />{selectedQaqcSpliceRecord.fibre_identification_verified || "-"}</div>
+      <div><strong>Splice Loss / Test Results Acceptable:</strong><br />{selectedQaqcSpliceRecord.splice_loss_acceptable || "-"}</div>
       <div><strong>Inspection Result:</strong><br />{selectedQaqcSpliceRecord.inspection_result || "-"}</div>
       <div><strong>Status:</strong><br />{selectedQaqcSpliceRecord.inspection_status || "-"}</div>
       <div><strong>Assigned To:</strong><br />{selectedQaqcSpliceRecord.corrective_action_assigned_to || "-"}</div>
