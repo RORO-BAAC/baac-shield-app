5cad683c6d440de79e69f91de64205186f2eef76
RORO-BAAC <rod.gonzalez@baacconstruction.com>
2026-09-08 09:52:14 -0600

    Update page.js

--- baac-shield-app-fixed/baac-next-fix/app/page.js
+++ baac-shield-app-fixed/baac-next-fix/app/page.js
@@ -17435,6 +17435,26 @@ onChange={(e) =>
         <option value="N/A">N/A</option>
       </select>
     </label>
+        <label style={{ display: "grid", gap: 6 }}>
+  <span style={{ fontWeight: "bold", color: "#334155" }}>
+    Approved Splice Plan / Fibre Assignment Verified?
+  </span>
+  <select
+    value={qaqcSplicePlanVerified}
+    onChange={(e) => setQaqcSplicePlanVerified(e.target.value)}
+    style={{
+      padding: 11,
+      borderRadius: 10,
+      border: "1px solid #cbd5e1",
+      background: "white",
+    }}
+  >
+    <option value="">Select</option>
+    <option value="Yes">Yes</option>
+    <option value="No">No</option>
+    <option value="N/A">N/A</option>
+  </select>
+</label>    
   </div>
 </div>
 
