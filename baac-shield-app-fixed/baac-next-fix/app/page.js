7cfee5a7139ce456b0dafcf16c3b48cd537896a8
RORO-BAAC <rod.gonzalez@baacconstruction.com>
2026-09-11 08:22:01 -0600

    Update page.js

--- baac-shield-app-fixed/baac-next-fix/app/page.js
+++ baac-shield-app-fixed/baac-next-fix/app/page.js
@@ -694,6 +694,7 @@ const [qaqcEquipmentSerial, setQaqcEquipmentSerial] = useState("");
 const [qaqcEquipmentVisualCondition, setQaqcEquipmentVisualCondition] = useState("");
 const [qaqcEquipmentInstallationAcceptable, setQaqcEquipmentInstallationAcceptable] = useState("");
 const [qaqcEquipmentMountedSecure, setQaqcEquipmentMountedSecure] = useState("");      
+ const [qaqcEquipmentGroundingBondingComplete, setQaqcEquipmentGroundingBondingComplete] = useState("");     
 const [qaqcEquipmentResult, setQaqcEquipmentResult] = useState("");
 const [qaqcEquipmentDeficiencies, setQaqcEquipmentDeficiencies] = useState("");
 const [qaqcEquipmentNotes, setQaqcEquipmentNotes] = useState("");
