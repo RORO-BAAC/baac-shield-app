0a63bf6e781f7ee58aa2409010b57fb10ebaab7b
RORO-BAAC <rod.gonzalez@baacconstruction.com>
2026-09-18 10:45:34 -0600

    Update page.js

--- baac-shield-app-fixed/baac-next-fix/app/page.js
+++ baac-shield-app-fixed/baac-next-fix/app/page.js
@@ -18831,6 +18831,27 @@ onChange={(e) =>
   </select>
 </label>
   <label>
+   <label>
+  Grounding / Bonding Complete?
+  <select
+    value={qaqcEquipmentGroundingBondingComplete}
+    onChange={(e) =>
+      setQaqcEquipmentGroundingBondingComplete(e.target.value)
+    }
+    style={{
+      width: "100%",
+      marginTop: 6,
+      padding: 10,
+      borderRadius: 8,
+      border: "1px solid #cbd5e1",
+    }}
+  >
+    <option value="">Select</option>
+    <option value="Yes">Yes</option>
+    <option value="No">No</option>
+    <option value="N/A">N/A</option>
+  </select>
+</label>     
     Overall Result
     <select
       value={qaqcEquipmentResult}
